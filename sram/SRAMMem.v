17fa897f0eb35049250ff97fec5916effd4d7151
yb265 <yb265@cornell.edu>
2025-11-02 14:58:13 -0500

    Converted Blimp memory req/resp messages to c2s2 sram req/resp messages

--- sram/SRAMMem.v
+++ sram/SRAMMem.v
@@ -6,6 +6,7 @@
 
 // not sure if this path resolves...
 `include "ip/sram/rtl/sram_minion.sv"
+`include "ip/sram/rtl/mem-msgs.sv"
 
 module SRAMMem #(
   parameter p_opaq_bits = 8
@@ -17,18 +18,35 @@ module SRAMMem #(
   MemNetResp.server resp
 );
 
+  mem_resp_4B_t minion_reqstream_msg;
+  mem_req_4B_t  minion_respstream_msg;
+
   sram_SRAMMinion sram_minion (
     .clk        (clk),
     .reset      (rst),
 
     .minion_reqstream_val(req.val),
     .minion_reqstream_rdy(req.rdy),
-    .minion_reqstream_msg(req.msg),
+    .minion_reqstream_msg(minion_reqstream_msg),
 
     .minion_respstream_val(resp.val),
     .minion_respstream_rdy(resp.rdy),
-    .minion_respstream_msg(resp.msg)
+    .minion_respstream_msg(minion_respstream_msg)
   );
+
+  // Convert from MemNetReq to minion_reqstream_msg
+  assign minion_reqstream_msg.type_ = req.msg.op;
+  assign minion_reqstream_msg.opaque = req.msg.opaque;
+  assign minion_reqstream_msg.addr  = req.msg.addr;
+  assign minion_reqstream_msg.strb  = req.msg.strb;
+  assign minion_reqstream_msg.data  = req.msg.data;
+
+  // Convert from minion_respstream_msg to MemNetResp
+  assign resp.msg.op      = minion_respstream_msg.type_;
+  assign resp.msg.opaque  = minion_respstream_msg.opaque;
+  assign resp.msg.addr    = minion_respstream_msg.addr;
+  assign resp.msg.strb    = minion_respstream_msg.strb;
+  assign resp.msg.data    = minion_respstream_msg.data;
 
 endmodule
 
