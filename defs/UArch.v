3a9f1258ca5c3671dea2f97fa6af840320fc3f9a
eyl47 <eyl47@cornell.edu>
2026-03-15 15:13:58 -0400

    fix merge

--- defs/UArch.v
+++ defs/UArch.v
@@ -14,7 +14,7 @@ package UArch;
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   // A linearization of opcodes to indicate a specific instruction type
 
-  parameter num_ops = 47;
+  parameter num_ops = 48;
 
   typedef enum logic [$clog2(num_ops)-1:0] {
     // Arithmetic
