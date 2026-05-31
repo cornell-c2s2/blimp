fae7750afe28bba1766d173087a835eb318ef0e9
simeon-turner <simeon.turner34@gmail.com>
2026-05-31 03:16:22 -0400

    bug fix to seq num gen

--- hw/fetch/SeqNumGenL3.v
+++ hw/fetch/SeqNumGenL3.v
@@ -100,7 +100,7 @@ module SeqNumGenL3 #(
   //----------------------------------------------------------------------
 
   // Can only allocate if we're not about to wrap around
-  assign alloc_val = !( curr_head_ptr + 1 == curr_tail_ptr );
+  assign alloc_val = ( (curr_tail_ptr == 0) ? !(curr_head_ptr == '1) !(curr_head_ptr + 1 == curr_tail_ptr) );
 
   assign is_alloc = alloc_val & alloc_rdy;
   assign alloc_seq_num = ( squash.val ) ? squash.seq_num + 1
